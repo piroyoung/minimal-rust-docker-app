21e0af849b0754856dae9d9b8d635652eb278415
piroyoung <mizukami_hiroki@cyberagent.co.jp>
2019-09-23 23:36:10 +0900

    with mod

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,17 @@
+use shape::Polygon;
+use shape::Rectangle;
+
+mod shape;
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle {
+        x: 0.0,
+        y: 0.0,
+        width: 10.0,
+        height: 10.0,
+    };
+
+    for point in rect.get_vertices().iter() {
+        println!("{0},{1}", point[0], point[1])
+    }
 }
---
+++ hello_world/src/shape.rs
@@ -0,0 +1,21 @@
+pub(crate) struct Rectangle {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+pub trait Polygon {
+    fn get_vertices(&self) -> Vec<Vec<f32>>;
+}
+
+impl Polygon for Rectangle {
+    fn get_vertices(&self) -> Vec<Vec<f32>> {
+        let mut points: Vec<Vec<f32>> = Vec::new();
+        points.push(vec![self.x, self.y]);
+        points.push(vec![self.x + self.width, self.y]);
+        points.push(vec![self.x + self.width, self.y + self.height]);
+        points.push(vec![self.x, self.y + self.height]);
+        points
+    }
+}
